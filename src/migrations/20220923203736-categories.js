fe80a0896891429c09af805a38eccb556f69492d
Jessy Damasceno <jessy_bas@hotmail.com>
2022-09-23 17:39:12 -0300

    chore: create categories migration

---
+++ src/migrations/20220923203736-categories.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
