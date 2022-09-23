ba39a0520d385e21f051a6d23eeeccc48916f1c4
Jessy Damasceno <jessy_bas@hotmail.com>
2022-09-23 17:36:52 -0300

    chore: create users migration

---
+++ src/migrations/20220923192702-users.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable('users', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+      display_name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      image: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      }
+		});
+	},
+
+	down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+	},
+};
