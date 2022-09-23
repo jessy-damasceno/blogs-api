a0ccd9f55e067292470beb177cb2ad33686b0dcd
Jessy Damasceno <jessy_bas@hotmail.com>
2022-09-23 17:41:46 -0300

    fix: categories migration

--- src/migrations/20220923203736-categories.js
+++ src/migrations/20220923203736-categories.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('users', {
+    await queryInterface.createTable('categories', {
       id: {
 				allowNull: false,
 				autoIncrement: true,
@@ -17,6 +17,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('users');
+    await queryInterface.dropTable('categories');
   }
 };
