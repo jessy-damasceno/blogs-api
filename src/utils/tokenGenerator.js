698a9378a87899e0a50496b7326e819a83d6d6b2
Jessy Damasceno <jessy_bas@hotmail.com>
2022-09-26 20:10:18 -0300

    fix: tokenGenerator

--- src/utils/tokenGenerator.js
+++ src/utils/tokenGenerator.js
@@ -2,12 +2,6 @@ const jwt = require('jsonwebtoken');
 
 require('dotenv/config');
 
-/* Sua chave secreta. É com ela que os dados do seu usuário serão encriptados.
-
-   Em projetos reais, armazene-a numa variável de ambiente e tenha cuidado com ela, pois só quem tem acesso
-
-   a ela poderá criar ou alterar tokens JWT. */
-
 const secret = process.env.JWT_SECRET;
 
 module.exports = (email) => {
