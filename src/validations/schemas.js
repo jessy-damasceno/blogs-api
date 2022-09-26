469a807985d63a191f71a73b2bf25576ddd6af11
Jessy Damasceno <jessy_bas@hotmail.com>
2022-09-26 20:43:16 -0300

    fix: newUserSchema

--- src/validations/schemas.js
+++ src/validations/schemas.js
@@ -4,6 +4,7 @@ const addUserSchema = Joi.object({
   displayName: Joi.string().min(8).required(),
   email: Joi.string().email({ tlds: { allow: false } }).required(),
   password: Joi.string().min(6).required(),
+  image: Joi.string(),
 });
 
 module.exports = {
